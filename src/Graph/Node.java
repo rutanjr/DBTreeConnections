af76441a52f274b172720530372f8c13207e186e
Hampus Dahlin <hampus.dahlin@gmail.com>
2016-07-13 11:09:44 +0200

    initial version of Node

--- src/Graph/Node.java
+++ src/Graph/Node.java
@@ -1,7 +1,28 @@
 package Graph;
 
+import java.util.ArrayList;
+
 /**
  * Created by rutanjr on 2016-07-13.
  */
 public class Node {
+    private ArrayList<Edge> edges = new ArrayList<Edge>();
+    private int id;
+
+    public Node(ArrayList<Edge> edges, int id){
+        this.edges = edges;
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public ArrayList<Edge> getEdges(){
+        return edges;
+    }
+
+    public int getNoEdges() {
+        return edges.size();
+    }
 }
